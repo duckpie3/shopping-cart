48d73778d890ae610240f59cf97ee97b328ef340
David <jdu@ciencias.unam.mx>
2026-06-06 16:00:52 -0600

    Agrega Registry service y config service de nuevo

--- services/config-server/src/test/java/com/config_server/ConfigServerApplicationTests.java
+++ services/config-server/src/test/java/com/config_server/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.config;
+package com.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
